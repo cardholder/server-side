44787ce748f25e2fc76ab40ef92d60ce403c4f76
Stefan Kroeker <stefan.kroeker@fh-bielefeld.de>
2019-05-25 12:41:51 +0200

    Use Dictionary instead of set for lobby list

--- src/lobby/lobby_list_handler.py
+++ src/lobby/lobby_list_handler.py
@@ -3,22 +3,30 @@ import random
 import string
 from .lobby import Lobby
 
-lobbylist = set()
+lobby_list = {}
 
 
 def create_lobby(game, visibility, max_players):
     # generates a random id for new lobby
     lobby_id = generate_id()
+    while check_if_lobby_exists(lobby_id):
+        # generates a new lobby_id if it already exists
+        lobby_id = generate_id()
+
     lobby = Lobby(lobby_id, game, visibility, max_players)
-    lobbylist.add(lobby)
+    lobby_list[str(lobby_id)] = lobby
     return lobby_id
 
 
-def remove_lobby(lobby):
-    lobbylist.remove(lobby)
+def remove_lobby(lobby_id):
+    del lobby_list[str(lobby_id)]
 
 
 def generate_id(string_length=7):
     """Generate a random string of fixed length """
     letters = string.ascii_letters + string.digits
     return ''.join(random.choice(letters) for i in range(string_length))
+
+
+def check_if_lobby_exists(lobby_id):
+    return str(lobby_id) in lobby_list

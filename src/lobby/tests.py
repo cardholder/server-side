00f597b6fad9c135db8c30d40e24e3de11afc3fb
Stefan Kroeker <stefan.kroeker@fh-bielefeld.de>
2019-05-27 16:18:10 +0200

    Write test

--- src/lobby/tests.py
+++ src/lobby/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class ChatTests(TestCase):
+
+    def test_when_chat_message_posted_then_seen_by_everyone_in_same_room(self):
+        try:
+            var = True
+            assert True == var
+
+        finally:
+            pass

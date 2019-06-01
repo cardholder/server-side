969ca45e2632399db5abbfe2647a5ce24bc1b08c
Stefan Kroeker <stefan.kroeker@fh-bielefeld.de>
2019-06-01 15:11:01 +0200

    test lobby object send to group_send

--- lobby/lobby_list_handler.py
+++ lobby/lobby_list_handler.py
@@ -39,20 +39,18 @@ def update_lobby(lobby_id):
     channel_layer = get_channel_layer()
     lobby = lobby_list[str(lobby_id)]
     print("\n\n\n\n\n\n")
-    print(str(lobby))
+    print(lobby)
     async_to_sync(channel_layer.group_send)(
         lobby_id,
-        {"type": "update.lobby", "lobby": lobby.__dict__},
+        {"type": "update.lobby", "lobby": lobby},
     )
     async_to_sync(channel_layer.group_send)(
         "lobbylist",
-        {"type": "update.lobby", "lobby": lobby.__dict__},
+        {"type": "update.lobby", "lobby": lobby},
     )
 
 
 def add_player_to_lobby(lobby_id, name):
-    print("add Player")
-    print(name)
     lobby = lobby_list[str(lobby_id)]
     player_id = get_highest_player_id_of_lobby(lobby)
     role = "player"

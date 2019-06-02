7a52bbb141c734fa159f491e8cb35aca03e2bf96
Stefan Kroeker <stefan.kroeker@fh-bielefeld.de>
2019-06-02 16:06:08 +0200

    Set up models for database

--- lobby/models.py
+++ lobby/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Card(models.Model):
+    value = models.CharField(max_length=200)
+    symbol = models.CharField(max_length=1)
+
+    def __str__(self):
+        return str(self.value) + str(self.symbol)
+
+
+class CardSet(models.Model):
+    name = models.CharField(max_length=200)
+    cards = models.ManyToManyField(Card)
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    name = models.CharField(max_length=200)
+    show_name = models.CharField(max_length=200)
+    max_players = models.IntegerField(default=4)
+    card_set = models.ForeignKey(CardSet, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name

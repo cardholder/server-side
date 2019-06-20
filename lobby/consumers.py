0102210e3767a5a415de16b13d6dbb09ce3ced72
Stefan Kroeker <stefan.kroeker@fh-bielefeld.de>
2019-06-20 14:46:55 +0200

    test receiving message

--- lobby/consumers.py
+++ lobby/consumers.py
@@ -202,11 +202,12 @@ class MauMauConsumer(WebsocketConsumer):
     # Receive message from WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        print(text_data_json)
         key = list(text_data_json.keys())
         if key[0] == "player_id":
             player_id = text_data_json["player_id"]
+            print(player_id)
             self.player = get_player_of_lobby(self.room_group_name, player_id)
+            print(self.player)
             if self.player is None:
                 self.disconnect(1000)
             else:
